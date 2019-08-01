a30425d54d9f05406293da636088495c4b84204d
Mark <mark.pyankov.mark@gmail.com>
2019-08-01 20:39:25 +0700

    UDP1

--- vkbot.js
+++ vkbot.js
@@ -1,10 +1,16 @@
 const { VK } = require('vk-io'); 
+var Tykaev_balance = 10000000; //https://vk.com/mishatukaev Михаил Тюкаев
+var Tykaev_factory = 0; //https://vk.com/mishatukaev Михаил Тюкаев 
+var Tykaev_army = 0; //https://vk.com/mishatukaev Михаил Тюкаев
+var Tykaev_tanks = 0; //https://vk.com/mishatukaev Михаил Тюкаев
+var Tykaev_oil = 0; //https://vk.com/mishatukaev Михаил Тюкаев 
 
-var Vitya_balance = 1000000; //https://vk.com/id299729466 Викьло мишин 
+var Vitya_balance = 10000000; //https://vk.com/id299729466 Викьло мишин 
 var Vitya_factory = 0; //https://vk.com/id299729466 Виктор мишин 
 var Vitya_army = 0; //https://vk.com/id299729466 Виктор мишин 
 var Vitya_tanks = 0; //https://vk.com/id299729466 Виктор мишин 
 var Vitya_oil = 0; //https://vk.com/id299729466 Виктор мишин 
+
 var factory = 500000;
 var oil_station = 500000;
 
@@ -33,6 +39,7 @@ vk.updates.hear(/2554887/i, context => {
     context.send(Vitya_tanks + "  <---Танковые рекруты")
     context.send(Vitya_oil + "  <---Нефтяные вышки")
     context.send(Vitya_oil*100000 + "  <---Прибыль")
+    context.send("Внимание,запущен тестовый режим")
 }); 
 
 //Покупка Виктор
@@ -48,7 +55,6 @@ if (Vitya_balance < 500000){
     context.send("Недостаточно средств") 
 } 
 });
-//oil 
 vk.updates.hear(/buy 2554882/i, context => { 
     if (Vitya_balance > 500000){
         Vitya_balance = Vitya_balance - oil_station;
@@ -62,9 +68,55 @@ vk.updates.hear(/buy 2554882/i, context => {
     }); 
 //КОНЕЦ Виктор////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+//НАЧАЛО МИША////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+vk.updates.hear(/89964535/i, context => { 
+    context.send('Игрок: Mihail Tykaev') 
+    context.send("Страна: None")
+    context.send("Правитель: None")
+    context.send("None")
+    context.send("None")
+    context.send(Tykaev_balance + "  <---Баланс")
+    context.send(Tykaev_factory + "  <---Фабрики")
+    context.send(Tykaev_army + "  <---Рекруты")
+    context.send(Tykaev_tanks + "  <---Танковые рекруты")
+    context.send(Tykaev_oil + "  <---Нефтяные вышки")
+    context.send(Tykaev_oil*100000 + "  <---Прибыль")
+    context.send("Внимание,запущен тестовый режим")
+    
+}); 
+
+//Покупка МИША
+
+vk.updates.hear(/buy 89964531/i, context => { 
+    if (Tykaev_balance > 500000){
+        Tykaev_balance = Tykaev_balance - factory;
+        Vitya_factory++;
+        context.send("Успешно купленно")}
+        
+    if (Tykaev_balance < 500000){
+     
+        context.send("Недостаточно средств") 
+    } 
+    });
+    //oil 
+    vk.updates.hear(/buy 89964532/i, context => { 
+        if (Tykaev_balance > 500000){
+            Tykaev_balance = Tykaev_balance - oil_station;
+            Vitya_oil++;
+            context.send("Успешно купленно")}
+    
+            if (Tykaev_balance < 500000){
+     
+                context.send("Недостаточно средств") 
+            } 
+        }); 
+//КОНЕЦ МИША////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Выдача новых ресурсов/Рекрутов
 vk.updates.hear(/9084b718230f172bde6de1ed2ab01423bf77cd1ad68bfae62638e1529e4266ced52ee9fb18190285a56e7/i, context => { 
     Vitya_balance = Vitya_balance + Vitya_oil*100000;
+    Tykaev_balance = Tykaev_balance + Tykaev_oil*100000;
     context.send("Готово пидор")
 });
 vk.updates.startPolling().then(() =>{ 
